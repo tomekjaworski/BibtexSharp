28cdbbc431d671b14916f3e556ea105530f1e798
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2015-10-24 14:43:06 +0200

    Project import

---
+++ BibtexSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
